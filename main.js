76936fe14da78394b2f1cb251ba57a019f1e1937
Abiy Melaku <agirma08@gmail.com>
2017-03-13 10:37:29 -0700

    Design board
    add one function to ask player for move

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ var gameBoard = [
   [" ", " ", " "]
 ];
 
+
+//Player's co-ordinates as an array
+var currentMove = [];
+var move = [];
+
+//function to get move of current player
+var askMove = function() {
+  console.log("Enter your move in the this format: # #")
+}
